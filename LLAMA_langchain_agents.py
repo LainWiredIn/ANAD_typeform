6a3eb73b9377563eff75a13be12ae4322e506e91
Manav <73900947+LainWiredIn@users.noreply.github.com>
2023-10-08 14:14:09 +0530

    Create LLAMA_langchain_agents.py

---
+++ LLAMA_langchain_agents.py
@@ -0,0 +1,59 @@
+from torch import cuda, bfloat16
+import transformers
+
+model_id = 'meta-llama/Llama-2-7b-chat-hf'
+
+device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
+
+# quantization to load large model with less GPU memory
+# requires the `bitsandbytes` library
+bnb_config = transformers.BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type='nf4',
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=bfloat16
+)
+
+# begin initializing HF items, need auth token for these
+hf_auth = '<insert hf auth token>'
+model_config = transformers.AutoConfig.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth
+)
+
+model = transformers.AutoModelForCausalLM.from_pretrained(
+    model_id,
+    trust_remote_code=True,
+    config=model_config,
+    quantization_config=bnb_config,
+    device_map='auto',
+    use_auth_token=hf_auth
+)
+model.eval()
+print(f"Model loaded on {device}")
+
+tokenizer = transformers.AutoTokenizer.from_pretrained(
+    model_id,
+    use_auth_token=hf_auth
+)
+
+generate_text = transformers.pipeline(
+    model=model, tokenizer=tokenizer,
+    return_full_text=True,  # langchain expects the full text
+    task='text-generation',
+    # we pass model parameters here too
+    temperature=0.3,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+    max_new_tokens=512,  # mex number of tokens to generate in the output
+    repetition_penalty=3.1  # without this output begins repeating
+)
+
+res = generate_text("How do I develop a conversational interface and implement multiple chat agents, each with a specific role such as convincing to give details, Extract and Verify user information and Format the extracted information")
+print(res[0]["generated_text"])
+
+from langchain.llms import HuggingFacePipeline
+
+llm = HuggingFacePipeline(pipeline=generate_text)
+
+llm(prompt="Beyond all ideas of right and wrong, Is there a field?")
+
+# Currently figuring out langchain agents
